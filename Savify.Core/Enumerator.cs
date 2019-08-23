d407823dbac1bc0d40e619ca01fb4adc97fd1a1c
Laurence Rawlings <rawlingslaurence@gmail.com>
2019-08-23 18:32:46 +0100

    Added Enumerator class to store custom variables for the youtube-dl args

---
+++ Savify.Core/Enumerator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Savify.Core
+{
+    public class Enumerator
+    {
+        public static string GetDescription(Enum value)
+        {
+            FieldInfo info = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])info.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            else
+            {
+                return value.ToString();
+            }                
+        }        
+    }
+
+    public enum Host
+    {
+        [Description("youtube.com")]
+        YouTube,
+        [Description("soundcloud.com")]
+        Soundcloud,
+        [Description("spotify.com")]
+        Spotify
+    }
+
+    public enum Status
+    {
+        [Description("In Queue")]
+        Waiting,
+        [Description("Downloading")]
+        Downloading,
+        [Description("Downloaded")]
+        Downloaded,
+        [Description("Failed")]
+        Failed
+    }
+
+    public enum Quality
+    {
+        [Description("32K")]
+        _32kbps = 32,
+        [Description("96K")]
+        _96kbps = 96,
+        [Description("128K")]
+        _128kbps = 128,
+        [Description("192K")]
+        _192kbps = 192,
+        [Description("256K")]
+        _256kbps = 256,
+        [Description("320K")]
+        _320kbps = 320,
+        [Description("9")]
+        lowest = 9,
+        [Description("0")]
+        highest = 0
+    }
+
+
+    public enum Format
+    {
+        [Description(".aac")]
+        aac,
+        [Description(".flac")]
+        flac,
+        [Description(".mp3")]
+        mp3,
+        [Description(".m4a")]
+        m4a,
+        [Description(".opus")]
+        opus,
+        [Description(".vorbis")]
+        vorbis,
+        [Description(".wav")]
+        wav//,
+        //best
+    }
+
+    public enum LinkType
+    {
+        [Description("Song")]
+        Song,
+        [Description("Playlist")]
+        Playlist,
+        [Description("Album")]
+        Album
+    }
+}
