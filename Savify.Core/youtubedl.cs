01db52a7ece9e5bd2776197f5623fe57f2a6e3fb
Laurence Rawlings <rawlingslaurence@gmail.com>
2019-08-23 18:54:01 +0100

    Added Youtubedl class to handle the youtube-dl process, also added a settings file

---
+++ Savify.Core/youtubedl.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Savify.Core
+{
+    public static class Youtubedl
+    {
+        public static Process GetProcess(string path, string args)
+        {
+            Process _youtubedl = new Process();
+            _youtubedl.StartInfo.UseShellExecute = false;
+            _youtubedl.StartInfo.FileName = path;
+            _youtubedl.StartInfo.Arguments = args;
+            _youtubedl.StartInfo.CreateNoWindow = true;
+            return _youtubedl;
+        }
+
+        public static void Update()
+        {
+            Process _youtubedl = GetProcess(Settings.Default.YouTubeDl, "-U");
+            _youtubedl.Start();
+            _youtubedl.WaitForExit();
+            _youtubedl.Close();
+        }
+    }
+}
