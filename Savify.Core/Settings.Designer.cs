fa02b2b6971cdd01a0f3b576eb0ee3bcf8215adf
Laurence Rawlings <rawlingslaurence@gmail.com>
2019-08-23 19:05:45 +0100

    Like Youtubedl class, added Ffmpeg class to hadle the FFmpeg process

--- Savify.Core/Settings.Designer.cs
+++ Savify.Core/Settings.Designer.cs
@@ -34,5 +34,17 @@ namespace Savify.Core {
                 this["YouTubeDl"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("ffmpeg.exe")]
+        public string FFmpeg {
+            get {
+                return ((string)(this["FFmpeg"]));
+            }
+            set {
+                this["FFmpeg"] = value;
+            }
+        }
     }
 }
