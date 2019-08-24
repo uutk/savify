a5243b2728f994c2f4e22f4bf7f9367a01a8bfc0
Laurence Rawlings <rawlingslaurence@gmail.com>
2019-08-24 16:42:23 +0100

    Added class for REst client

---
+++ Savify.Core/RestClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Savify.Core
+{
+    public enum httpVervb
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    public class RestClient
+    {
+        public string EndPoint { get; set; }
+        public httpVervb HttpMethod { get; set; }
+
+        public RestClient()
+        {
+            EndPoint = string.Empty;
+            HttpMethod = httpVervb.GET;
+        }
+
+        public string MakeRequest()
+        {
+            string strResponse = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(EndPoint);
+
+            request.Method = HttpMethod.ToString();
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new ApplicationException("Error Code: " + response.StatusCode.ToString());
+                }
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponse = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return strResponse;
+        }
+    }
+}
