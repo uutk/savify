c11781f3da76da19f2626373aa3a14679fdb38e1
Laurence Rawlings <rawlingslaurence@gmail.com>
2019-09-10 20:45:06 +0100

    Added new class DownloadManager.cs and moved some methods over from Song.cs

---
+++ Savify.Core/DownloadManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Savify.Core
+{
+    public static class DownloadManager
+    {
+        private readonly static string YOUTUBEDL_ARGS = @"--extract-audio --format bestaudio --audio-quality {0} --audio-format {1} --prefer-ffmpeg --ffmpeg-location ""{2}"" --continue --ignore-errors --no-overwrites ""{3}"" --default-search ""{4}""";
+        private readonly static string SONG_ARGS = @" --output ""{5}%(title)s.%(ext)s"" --no-playlist";
+        private readonly static string PLAYLIST_ARGS = @"""{0}"" --get-id --ignore-errors";
+        private readonly static string SPOTIFY_SONG_ARGS = @" --output ""{5}{6} - {7}.%(ext)s"" --no-playlist";
+        private readonly static string SPOTIFY_PLAYLIST_ARGS = @" --output ""{5}{6}\{7} - {8}.%(ext)s"" --no-playlist";
+        private readonly static string METADATA = @" --add-metadata --metadata-from-title ""(?P<artist>.+?) - (?P<title>.+)"" --xattrs";
+        private readonly static string RESTRICT_FILENAMES = @" --restrict-filenames";
+
+        public static void DownloadQuery(string query)
+        {
+            Uri link = GetLink(query);
+            string args;
+
+            if (link.OriginalString != string.Empty)
+            {
+                args = string.Format(YOUTUBEDL_ARGS + METADATA + SONG_ARGS + (Settings.Default.RestrictFilenames ? RESTRICT_FILENAMES : ""), Settings.Default.Quality, Enumerator.GetValueFromDescription<Format>(Settings.Default.Format), Settings.Default.FFmpeg, query, Settings.Default.Search, Path.GetTempPath());
+
+            }
+            else
+            {
+                Host host = GetHost(link);
+                LinkType linkType = GetLinkType(link, host);
+                if (host == Host.YouTube)
+                {
+                    if (linkType == LinkType.Playlist)
+                    {
+                        args = string.Format(PLAYLIST_ARGS, link.OriginalString);
+                        string output = Youtubedl.Run(args);
+
+                        List<string> ids = new List<string>();
+                        string[] stringSeparators = new string[] { "\n" };
+                        foreach (string id in output.Split(stringSeparators, StringSplitOptions.None))
+                        {
+                            if (id.Length == 11)
+                            {
+                                ids.Add(id);
+                            }
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private static LinkType GetLinkType(Uri link, Host host)
+        {
+
+
+            if (host == Host.Spotify)
+            {
+                if (link.OriginalString.Contains("/album/"))
+                    return LinkType.Album;
+                else if (link.OriginalString.Contains("/playlist/"))
+                    return LinkType.Playlist;
+                else if (link.OriginalString.Contains("/track/"))
+                    return LinkType.Song;
+                else
+                    throw new Exception("Not a valid spotify link.");
+            }
+            else if (host == Host.YouTube)
+            {
+                if (link.OriginalString.Contains("/watch?v="))
+                    return LinkType.Song;
+                else if (link.OriginalString.Contains("/playlist?list="))
+                    return LinkType.Playlist;
+                else
+                    throw new Exception("Not a valid youtube link.");
+            }
+            else if (host == Host.Soundcloud)
+            {
+                //Treat album like playlist
+                if (link.OriginalString.Contains("/sets/"))
+                    return LinkType.Playlist;
+                else
+                    return LinkType.Song;
+            }
+            else
+            {
+                throw new Exception("Not a valid link for that website.");
+            }
+        }
+
+        private static Uri GetLink(string query)
+        {
+            _ = Uri.TryCreate(query, UriKind.Absolute, out Uri uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            return uriResult;
+        }
+
+        private static Host GetHost(Uri link)
+        {
+            string host = link.Host;
+
+            if (host.Contains(Enumerator.GetDescription(Host.Spotify)))
+                return Host.Spotify;
+            else if (host.Contains(Enumerator.GetDescription(Host.YouTube)))
+                return Host.YouTube;
+            else if (host.Contains(Enumerator.GetDescription(Host.Soundcloud)))
+                return Host.Soundcloud;
+            else
+                throw new Exception("Link from that website are not supported.");
+        }
+    }
+}
--- Savify.Core/Song.cs
+++ Savify.Core/Song.cs
@@ -1,29 +1,31 @@
-﻿using System;
+﻿using MetaBrainz.MusicBrainz;
+using MetaBrainz.MusicBrainz.CoverArt;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using MetaBrainz.MusicBrainz;
-using MetaBrainz.MusicBrainz.CoverArt;
 
 namespace Savify.Core
 {
     public class Song
     {
-        public readonly string YOUTUBEDL_ARGS = @"--extract-audio --format bestaudio --audio-quality {0} --audio-format {1} --prefer-ffmpeg --ffmpeg-location ""{2}"" --continue --ignore-errors --no-overwrites ""{3}"" --default-search ""{4}""";
-        public readonly string SONG_ARGS = @" --output ""{5}%(title)s.%(ext)s"" --no-playlist";
-        public readonly string PLAYLIST_ARGS = @" --output ""{5}%(playlist)s\%(title)s.%(ext)s"" --yes-playlist";
-        public readonly string SPOTIFY_SONG_ARGS = @" --output ""{5}{6} - {7}.%(ext)s"" --no-playlist";
-        public readonly string SPOTIFY_PLAYLIST_ARGS = @" --output ""{5}{6}\{7} - {8}.%(ext)s"" --no-playlist";
-        public readonly string METADATA = @" --add-metadata --metadata-from-title ""(?P<artist>.+?) - (?P<title>.+)"" --xattrs";
-        public readonly string WRITE_THUMBNAIL = @" --write-thumbnail --embed-thumbnail";
-        public readonly string RESTRICT_FILENAMES = @" --restrict-filenames";
-        public readonly string FFMPEG_COVERART = @"-i ""{0}"" -i ""{1}"" -map 0:0 -map 1:0 -codec copy -id3v2_version 3 -metadata:s:v title=""Album cover"" -metadata:s:v comment=""Cover(front)"" ""{2}""";
-        public readonly string MUSICBRAINZ_HEAD = @"Savify/0.1.2 ( https://l4rry2k.github.io/savify/ )";
-        public readonly string FFMPEG_GET_METADATA = @" -i ""{0}""";
-        public readonly string FFMPEG_CLEAR_METADATA = @" -i ""{0}"" -map_metadata -1 ""{1}""";
-        public readonly string FFMPEG_WRITE_METADATA = @" -i ""{0}"" -metadata title=""{1}"" -metadata artist=""{2}"" -metadata album=""{3}"" -id3v2_version 3 -write_id3v1 1 ""{4}""";
+        private readonly string YOUTUBEDL_ARGS = @"--extract-audio --format bestaudio --audio-quality {0} --audio-format {1} --prefer-ffmpeg --ffmpeg-location ""{2}"" --continue --ignore-errors --no-overwrites ""{3}"" --default-search ""{4}""";
+        private readonly string SONG_ARGS = @" --output ""{5}%(title)s.%(ext)s"" --no-playlist";
+        private readonly string PLAYLIST_ARGS = @"""{0}"" --get-id --ignore-errors";
+        private readonly string SPOTIFY_SONG_ARGS = @" --output ""{5}{6} - {7}.%(ext)s"" --no-playlist";
+        private readonly string SPOTIFY_PLAYLIST_ARGS = @" --output ""{5}{6}\{7} - {8}.%(ext)s"" --no-playlist";
+        private readonly string METADATA = @" --add-metadata --metadata-from-title ""(?P<artist>.+?) - (?P<title>.+)"" --xattrs";
+        private readonly string RESTRICT_FILENAMES = @" --restrict-filenames";
+        private readonly string FFMPEG_COVERART = @"-i ""{0}"" -i ""{1}"" -map 0:0 -map 1:0 -codec copy -id3v2_version 3 -metadata:s:v title=""Album cover"" -metadata:s:v comment=""Cover(front)"" ""{2}""";
+        private readonly string MUSICBRAINZ_HEAD = @"Savify/0.1.2 ( https://l4rry2k.github.io/savify/ )";
+
+
+        //private readonly string PLAYLIST_ARGS = @" --output ""{5}%(playlist)s\%(title)s.%(ext)s"" --yes-playlist";
+        //private readonly string WRITE_THUMBNAIL = @" --write-thumbnail --embed-thumbnail";
+        //private readonly string FFMPEG_GET_METADATA = @" -i ""{0}""";
+        //private readonly string FFMPEG_CLEAR_METADATA = @" -i ""{0}"" -map_metadata -1 ""{1}""";
+        //private readonly string FFMPEG_WRITE_METADATA = @" -i ""{0}"" -metadata title=""{1}"" -metadata artist=""{2}"" -metadata album=""{3}"" -id3v2_version 3 -write_id3v1 1 ""{4}""";
 
         public string Search { get; set; }
         public string Title { get; set; }
@@ -41,108 +43,36 @@ namespace Savify.Core
             this.Status = Status.Waiting;
         }
 
-        private bool IsLink()
-        {
-            bool result = Uri.TryCreate(Search, UriKind.Absolute, out Uri uriResult)
-                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-
-            return result;
-        }
-
-        private Uri GetLink()
-        {
-            bool result = Uri.TryCreate(Search, UriKind.Absolute, out Uri uriResult)
-                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-
-            return uriResult;
-        }
-
-        private Host GetHost()
-        {
-            string host = GetLink().Host;
-
-            if (host.Contains(Enumerator.GetDescription(Host.Spotify)))
-                return Host.Spotify;
-            else if (host.Contains(Enumerator.GetDescription(Host.YouTube)))
-                return Host.YouTube;
-            else if (host.Contains(Enumerator.GetDescription(Host.Soundcloud)))
-                return Host.Soundcloud;
-            else
-                throw new Exception("Link from that website are not supported.");
-        }
-
-        private LinkType GetLinkType()
-        {
-            Host host = GetHost();
-            Uri link = GetLink();
-
-            if (host == Host.Spotify)
-            {
-                if (link.OriginalString.Contains("/album/"))
-                    return LinkType.Album;
-                else if (link.OriginalString.Contains("/playlist/"))
-                    return LinkType.Playlist;
-                else if (link.OriginalString.Contains("/track/"))
-                    return LinkType.Song;
-                else
-                    throw new Exception("Not a valid spotify link.");
-            }
-            else if (host == Host.YouTube)
-            {
-                if (link.OriginalString.Contains("/watch?v="))
-                    return LinkType.Song;
-                else if (link.OriginalString.Contains("/playlist?list="))
-                    return LinkType.Playlist;
-                else
-                    throw new Exception("Not a valid youtube link.");
-            }
-            else if (host == Host.Soundcloud)
-            {
-                //Treat album like playlist
-                if (link.OriginalString.Contains("/sets/"))
-                    return LinkType.Playlist;
-                else
-                    return LinkType.Song;
-            }
-            else
-            {
-                throw new Exception("Not a valid link for that website.");
-            }
-        }
-
         private Format GetFormat()
         {
             return Enumerator.GetValueFromDescription<Format>(Path.GetExtension(FileLocation.LocalPath));
         }
 
-        public void Download()
+        public void Download(string args)
         {
             Status = Status.Downloading;
-            string args;
-            if (!IsLink())
+            string output = Youtubedl.Run(args);
+
+            Match match = new Regex(@"(?<=\[ffmpeg\] Destination: )(.*?)(?=\n)").Match(output);
+
+            if (match.Success)
             {
-                args = string.Format(YOUTUBEDL_ARGS + METADATA + SONG_ARGS + (Settings.Default.RestrictFilenames ? RESTRICT_FILENAMES : ""), Settings.Default.Quality, Enumerator.GetValueFromDescription<Format>(Settings.Default.Format), Settings.Default.FFmpeg, Search, Settings.Default.Search, Path.GetTempPath());
-                string output = Youtubedl.Run(args);
+                Status = Status.Downloaded;
+                FileLocation = new Uri(match.Value);
 
-                Match match = new Regex(@"(?<=\[ffmpeg\] Destination: )(.*?)(?=\n)").Match(output);
-
-                if (match.Success)
+                if (GetFormat() == Format.mp3)
                 {
-                    Status = Status.Downloaded;
-                    FileLocation = new Uri(match.Value);
-
-                    if (GetFormat() == Format.mp3)
-                    {
-                        ReadMetadada();
-                        DownloadAlbumCover();
-                        WriteMetadata();
-                    }
-
-                    Rename();
-
-                    Console.WriteLine("\nDownloaded: " + FileLocation.LocalPath + " [RETURN]");
+                    ReadMetadada();
+                    DownloadAlbumCover();
+                    WriteMetadata();
                 }
+
+                Rename();
+
+                Console.WriteLine("\nDownloaded: " + FileLocation.LocalPath + " [RETURN]");
             }
+
+            
         }
 
         private void DownloadAlbumCover()
